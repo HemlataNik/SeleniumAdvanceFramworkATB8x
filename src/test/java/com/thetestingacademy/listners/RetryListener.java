ac655366dd63133143e7771b9cdd5f2f03d2cad1
HemlataNik <morehemlata36@gmail.com>
2025-01-10 17:22:10 +0530

    New framework added

---
+++ src/test/java/com/thetestingacademy/listners/RetryListener.java
@@ -0,0 +1,4 @@
+package com.thetestingacademy.listners;
+
+public class RetryListener {
+}
---
+++ src/test/java/com/thetestingacademy/tests/vwoLogin/TestVWOLogin_POM.java
@@ -0,0 +1,47 @@
+package com.thetestingacademy.tests.vwoLogin;
+
+import com.thetestingacademy.base.CommonToAllTest;
+import com.thetestingacademy.driver.DriverManager;
+import com.thetestingacademy.pages.pageObjectModel.vwo.DashboardPage_POM;
+import com.thetestingacademy.pages.pageObjectModel.vwo.LoginPage_POM;
+import com.thetestingacademy.utils.PropertiesReader;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class TestVWOLogin_POM extends CommonToAllTest {
+
+
+    @Owner("PRAMOD")
+    @Description("Verify that invalid creds give error message")
+    @Test
+    public void testLoginNegativeVWO() {
+
+        LoginPage_POM loginPagePom = new LoginPage_POM(DriverManager.getDriver());
+        String error_msg = loginPagePom.loginToVWOLoginInvalidCreds(PropertiesReader.readKey("invalid_username"), PropertiesReader.readKey("invalid_password"));
+
+        assertThat(error_msg).isNotBlank().isNotNull().isNotEmpty();
+        Assert.assertEquals(error_msg, PropertiesReader.readKey("error_message"));
+
+
+    }
+
+    @Owner("PRAMOD")
+    @Description("Verify that invalid creds give error message")
+    @Test
+    public void testPositive() {
+
+        LoginPage_POM loginPagePom = new LoginPage_POM(DriverManager.getDriver());
+        loginPagePom.loginToVWOLoginValidCreds(PropertiesReader.readKey("username"), PropertiesReader.readKey("password"));
+
+        DashboardPage_POM dashboardPagePom = new DashboardPage_POM(DriverManager.getDriver());
+        String loggedInUserName = dashboardPagePom.loggedInUserName();
+
+        assertThat(loggedInUserName).isNotBlank().isNotNull().isNotEmpty();
+        Assert.assertEquals(loggedInUserName, PropertiesReader.readKey("expected_username"));
+
+
+    }
+}
